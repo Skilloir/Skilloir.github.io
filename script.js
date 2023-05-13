3944cbcd07d8d86c189a7425fc7a9e2a19f349d8
Skilloir <95062499+Skilloir@users.noreply.github.com>
2023-05-13 00:07:22 -0500

    Cookie Clicker Update 5/13

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function darkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+const cookieAmount = 0
+const gmPrice = 50
+const farmPrice = 500
+
+function cookieClick() {
+    const amount = document.getElementById('amt')
+    const cookieAmount = cookieAmount + 1
+    console.log(cookieAmount)
 }
