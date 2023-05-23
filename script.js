96678224690a5e442662ac22f6a4736db0bb4440
Skilloir <95062499+Skilloir@users.noreply.github.com>
2023-05-22 22:16:42 -0500

    test

--- script.js
+++
@@ -1,9 +0,0 @@
-const cookieAmount = 0
-const gmPrice = 50
-const farmPrice = 500
-
-function cookieClick() {
-    const amount = document.getElementById('amt')
-    const cookieAmount = cookieAmount + 1
-    console.log(cookieAmount)
-}
